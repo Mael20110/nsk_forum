5e6b5e5e481725a2e5e058d669acf5d56c6d1af8
Mael20110 <maelfraissigne@gmail.com>
2026-05-20 13:34:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 const form = document.getElementById("contactForm");
+const status = document.getElementById("status");
 
+// mots interdits
+const bannedWords = ["spam", "hack", "arnaque", "insulte"];
+
+// envoyer message
 form.addEventListener("submit", function(e){
-
   e.preventDefault();
 
-  const message =
-    document.getElementById("message").value;
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
 
+  // règle longueur minimum
   if(message.length < 20){
-    alert("Minimum 20 caractères");
+    status.innerText = "❌ Minimum 20 caractères";
     return;
   }
 
-  alert("Message envoyé !");
+  // filtre mots interdits
+  if(bannedWords.some(word => message.toLowerCase().includes(word))){
+    status.innerText = "❌ Message refusé (mot interdit)";
+    return;
+  }
+
+  // objet message
+  const newMessage = {
+    name,
+    email,
+    message,
+    date: new Date().toLocaleString(),
+    response: ""
+  };
+
+  // récupérer messages existants
+  let messages = JSON.parse(localStorage.getItem("messages")) || [];
+
+  messages.push(newMessage);
+
+  localStorage.setItem("messages", JSON.stringify(messages));
+
+  status.innerText = "✅ Message envoyé !";
+
+  form.reset();
 });
